3dfc804210d26fde59d8cc5116a9e0d0be555cd6
Anastasiia Shadrinova <nastya.shadrinova@yandex.ru>
2020-08-06 22:31:46 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo 'Hello'; ?></title>
+</head>
+<body>
+	<?php
+		echo 'Hello World!';
+	?>
+</body>
+</html>
